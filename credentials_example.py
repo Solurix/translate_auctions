f3774e7c2b2fb8c92a7745674a5691d0b77e7083
Bartosz <g.rapnal@gmail.com>
2021-08-03 20:46:36 +0900

    renamed

--- credentials_example.py
+++
@@ -1,5 +0,0 @@
-ORG_EMAIL   = "@gmail.com"
-FROM_EMAIL  = "yourEmailAddress" + ORG_EMAIL
-FROM_PWD    = "yourPassword"
-SMTP_SERVER = "imap.gmail.com"
-SMTP_PORT   = 993
---
+++ settings_example.py
@@ -0,0 +1,4 @@
+EMAIL = "email@gmail.com"
+PASSWORD = "pw"
+SMTP_SERVER = "imap.gmail.com"
+EMAIL_TO = ", ".join([None, None])
