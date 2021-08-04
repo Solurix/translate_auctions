b46f32a2003dd809441ee5aecad1ee5f3207be51
Bartosz <g.rapnal@gmail.com>
2021-08-04 20:26:14 +0900

    updated settings

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ import os
 EMAIL = os.getenv("EMAIL")
 PASSWORD = os.getenv("PASSWORD")
 SMTP_SERVER = os.getenv("SMTP_SERVER")
-EMAIL_TO = eval(os.getenv("SMTP_SERVER"))
+EMAIL_TO = eval(os.getenv("EMAIL_TO"))
 EMAIL_FROM = EMAIL
 PASSWORD_FROM = PASSWORD
 FETCH_TIME = 24 * 3600 # fetch mails that are no older than this value (seconds)
